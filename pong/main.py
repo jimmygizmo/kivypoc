0c3f19c5aa86c749b30efcf47a1d637679cfa486
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-06-09 02:10:01 -0700

    basic game is working now. might need a little more randomization when serving.

--- pong/main.py
+++ pong/main.py
@@ -14,8 +14,22 @@ from random import randint
 ########################################################################################################################
 
 
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            # TODO: The .x and .y properties below show a warning why needs to be resolved before considering ignoring.
+            #   Error: Unresolved attribute reference 'x' for class 'list' .. (same for 'y')
+            ball.velocity = vel.x, vel.y + offset
+
+
 class PongBall(Widget):
-    # velocity of the ball on x and y axis
+    # Velocity of the ball on x and y axis
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
 
@@ -31,28 +45,50 @@ class PongBall(Widget):
 
 class PongGame(Widget):
     ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
 
-    def serve_ball(self):
+    def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
-        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+        #self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+        self.ball.velocity = vel
 
     def update(self, dt):
         self.ball.move()
+
+        # bounce of paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
 
         # bounce off top and bottom
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
 
-        # bounce off left and right
-        if (self.ball.x < 0) or (self.ball.right > self.width):
-            self.ball.velocity_x *= -1
+        # # bounce off left and right
+        # if (self.ball.x < 0) or (self.ball.right > self.width):
+        #     self.ball.velocity_x *= -1
+
+        # went off to a side to score point?
+        if self.ball.x < self.x:
+            self.player2.score += 1
+            self.serve_ball(vel=(4, 0))
+        if self.ball.x > self.width:
+            self.player1.score += 1
+            self.serve_ball(vel=(-4, 0))
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width / 3:
+            self.player2.center_y = touch.y
 
 
 class PongApp(App):
     def build(self):
         pong_game = PongGame()
         pong_game.serve_ball()
-        Clock.schedule_interval(pong_game.update, 1.0/60.0)  # Clock will call update 60 times per second
+        # Clock will call update 60 times per second
+        Clock.schedule_interval(pong_game.update, 1.0/60.0)
         return pong_game
 
 
